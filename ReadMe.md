7727e52820e7ee50a7ce77e2151a438870ef0f32
Felix Brüning <fbrning@uni-bremen.de>
2021-10-27 21:24:41 +0200

    Added parser for cafe unique meal-plan

--- ReadMe.md
+++ ReadMe.md
@@ -1,12 +1,22 @@
-# MensaPlan-CLI 
-This python-script prints the todays mensa-meals of the Uni-Bremen Mensa of the central campus.
+# MensaPlan Python3 Script
+This python-script prints the todays meals of the Uni-Bremen Mensa or of the Cafe Unique on the central campus.
+
+Following options are available:
+
+    -h, --help      show this help message and exit 
+    -v, --verbose   shows only main meals 
+    -m, --mensa     prints meals of mensa uni bremen 
+    -u, --unique    prints meals of cafe unique 
+    -a, --show-all  prints meals of cafe unique and uni-mensa 
+
+
 
 # Required python3-libraries
 $ pip3 install `BeautifulSoup` 
 
 $ pip3 install `bs4`
 
-# Run
-$ python3 mensaplan.py
+# Usage
+$ python3 `mensaplan.py` [options]
 
 *It is recommended to pipe the output to `less` or `more` because the program output may be a bit long.*
--- mensaplan.py
+++ mensaplan.py
@@ -6,6 +6,7 @@
 ## Last changed  : 2021-10-12
 ## 
 ##
+
 # --------------------------------------------------------------
 from bs4 import BeautifulSoup
 import urllib.request as urllib2 
@@ -49,6 +50,7 @@ if options.verbose:
     verbose = True
 if options.uni_mensa:
     uni_mensa = True
+    cafe_unique = False
 if options.cafe_unique:
     cafe_unique = True
     uni_mensa = False
@@ -65,7 +67,6 @@ mensa_food_categories = ['Ausgabe 1',
                          'Cafe Central "to-go" Angebot 1', 
                          'Cafe Central "to-go" Angebot 2']
 
-# --------------------------------------------------------------
 # remove a weird html-tag from html-table section
 # no elegant solution but it works
 def remove_sup_section(section):
@@ -97,9 +98,53 @@ def print_mensa_meal():
         string += "\n --> Preis (Bedienstete): " + price_emp 
         print(str(string))
         print("\n")
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Cafe Unique Bremen Campus
+UNIQUE_MENU = "https://www.uni-bremen.de/speiseplaene"
+
+def print_unique_meal():
+    html_dump = urllib2.urlopen(UNIQUE_MENU)
+    soup = BeautifulSoup(html_dump, this_parser)
+    food_table = soup.findAll('tr', class_="tr-odd")
+    content = food_table[0]
+    sec_row = content.findAll(lambda item: item.name == 'td')    
+    parts = sec_row[1].text.split("€")
+
+    meals = []
+    for s in parts:
+        s = re.sub('[0-9,]', '', s)
+        meals.append(s)
+    
+    meals = [x for x in meals if x and x != " "]
+
+    # 'prices' is a list of orderes price-strings
+    allPrices = sec_row[1].findAll(lambda item: item.name == 'strong')
+    prices = []
+    for var in allPrices:
+        p2 = str(var)
+        prices.append(p2.replace("<strong>", "").replace("</strong>", ""))
+
+    if len(prices) != len(meals):
+        print("[ERROR] Length of price-row and meal-row are different!")
+        return
+
+    print("##########################################")
+    print("## Meal-plan Cafe Unique") 
+    print("##")
+    print("## Today's date: " + datetime.datetime.now().strftime("%A, den %d.%m.%Y"))
+    print("##")
+    print("##########################################\n")
+
+    for i in range(0, len(meals)):
+        print(meals[i])
+        print(" --> Preis: {0}\n".format(prices[i]))
+
+# --------------------------------------------------------------
 
 if show_all or uni_mensa:
     print_mensa_meal()
 
 if show_all or cafe_unique:
-    print("MEAL CAFE UNIQUE TEST")
+    print_unique_meal()
